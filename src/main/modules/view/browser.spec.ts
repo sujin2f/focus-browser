cd3b1577d3e4bad21c3fb02b87fc59aacf8de60e
Sujin Choi <sujin.2f@gmail.com>
2025-11-06 01:37:29 -0500

    Revert "Update browser.spec.ts"
    This reverts commit c4737a86960c2dac7ba3da596c9727fafd18b977.

--- src/main/modules/view/browser.spec.ts
+++ src/main/modules/view/browser.spec.ts
@@ -1,5 +1,6 @@
 import { electron, loadURL } from '@test/mock-electron'
 import { adBlocker } from '@test/mock-ad-blocker'
+import { window } from '@test/mock-window'
 import { util } from '@test/mock-main-util'
 import { history, popupBlocker, status, statusGet } from '@test/mock-store'
 
@@ -9,6 +10,7 @@ jest.doMock('@main/modules/adblocker-electron', adBlocker)
 jest.doMock('@main/modules/store/popup-blocker', popupBlocker)
 jest.doMock('@main/modules/store/history', history)
 jest.doMock('@main/modules/store/status', status)
+jest.doMock('@main/modules/window/window', window)
 jest.doMock('@main/util', util)
 
 import { BrowserView } from '@src/main/modules/view/browser'

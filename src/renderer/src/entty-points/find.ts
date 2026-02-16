afb8b3b2918667acbb3140e191361871c26c48e1
Sujin Choi <sujin.2f@gmail.com>
2026-02-16 14:10:57 -0500

    Finish Find

--- src/renderer/src/entty-points/find.ts
+++ src/renderer/src/entty-points/find.ts
@@ -1,6 +1,11 @@
 import { A_Entry } from '@src/renderer/src/entty-points/abs-entry'
 /* Utils */
-import { checkElectron, ipcRenderer, navigate } from '@src/renderer/src/utils'
+import {
+    checkElectron,
+    getSection,
+    ipcRenderer,
+    navigate,
+} from '@src/renderer/src/utils'
 /* <HTML Fragments /> */
 import { Input } from '@src/renderer/src/fragments/input'
 import { Button } from '@src/renderer/src/fragments/button'
@@ -8,24 +13,25 @@ import { Button } from '@src/renderer/src/fragments/button'
 import { IPC_CHANNELS, RequestHandler } from '@src/common/constants'
 
 class Find extends A_Entry {
+    private form: HTMLFormElement = getSection<HTMLFormElement>('form')
+
     constructor() {
         super()
 
-        new Input('Find in Page', 'search')
-            .prependTo('form')
-            .setOnEnter((e) => {
-                if (e.code === 'Enter') {
-                    ipcRenderer.send(
-                        IPC_CHANNELS.FIND,
-                        RequestHandler.REQUEST,
-                        (e.target as HTMLInputElement).value,
-                    )
-                    navigate()
-                }
-            })
+        const input = new Input('Find in Page', 'search')
+            .prependTo(this.form)
             .focus()
-        new Button('Cancel').appendTo('button')
-        new Button('Find').appendTo('button')
+        new Button('Cancel', 'button-hollow').appendTo('button').type = 'reset'
+        new Button('Find').appendTo('button').type = 'submit'
+
+        this.form.addEventListener('submit', () => {
+            ipcRenderer.send(
+                IPC_CHANNELS.FIND,
+                RequestHandler.REQUEST,
+                input.value,
+            )
+            navigate()
+        })
     }
 }
 
